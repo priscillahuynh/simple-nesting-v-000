00c501c4531de56a4fc3b8229f8b0605fe7582fd
Priscilla H <priscillamhuynh@gmail.com>
2018-06-26 11:18:18 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -56,7 +56,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-		 programmer_hash[:dennis_ritchie][:languages].to_a
+		 programmer_hash[:dennis_ritchie][:languages].join
 end
 
 def adding_matz
