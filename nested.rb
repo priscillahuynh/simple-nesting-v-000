00fd7aa4184db81045bea27fa53c9687161060ac
Priscilla H <priscillamhuynh@gmail.com>
2018-06-26 11:51:31 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -65,7 +65,6 @@ def adding_matz
 #   :known_for => "Ruby",
 #    :languages => ["LISP", "C"]
 # }
-
 	programmer_hash =
  		{
         :grace_hopper => {
@@ -82,7 +81,7 @@ def adding_matz
         }
      }
 
-		 programmer_hash[:yukihiro_matsumoto]= {:known_for=> "Ruby", :languages=>["LISP","C"]}
+		 programmer_hash[:yukihiro_matsumoto => {:known_for=> "Ruby", :languages=>["LISP","C"]}]
 end
 
 def changing_alan
